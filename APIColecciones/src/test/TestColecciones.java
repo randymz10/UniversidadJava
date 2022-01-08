71a65a372d90099128a4fdd05b98bf262e532dbb
Randy Muñoz <86855407+randymz10@users.noreply.github.com>
2022-01-08 16:46:17 -0300

    add proyect API Collections

---
+++ APIColecciones/src/test/TestColecciones.java
@@ -0,0 +1,37 @@
+package test;
+
+import java.util.*;
+
+public class TestColecciones {
+    public static void main(String[] args) {
+        List miLista = new ArrayList();
+        miLista.add("Lunes");
+        miLista.add("Martes");
+        miLista.add("Miercoles");
+        miLista.add("Jueves");
+        miLista.add("Viernes");
+        miLista.add("Sabado");
+        miLista.add("Domingo");
+        
+//        for (Object elemento : miLista) {
+//            System.out.println("elemento = " + elemento);
+//        }
+//        imprimir(miLista);
+        Set miSet = new HashSet();
+        miSet.add("Lunes");
+        miSet.add("Martes");
+        miSet.add("Mierccoles");
+        miSet.add("Jueves");
+        miSet.add("Viernes");
+        miSet.add("Sabado");
+        miSet.add("Domingo");
+        
+        imprimir(miSet);
+    }
+    
+    public static void imprimir(Collection coleccion ){
+        for (Object elemento : coleccion) {
+            System.out.println("elemento = " + elemento);
+        }
+    }
+}
