89f9732e98c0ef6404cdc31bb5d5490171222dca
Randy Muñoz <86855407+randymz10@users.noreply.github.com>
2022-01-28 23:02:51 -0300

    Add files via upload

---
+++ JDBC/IntroduccionJDBC/src/main/java/test/TestMySqlJDBC.java
@@ -0,0 +1,33 @@
+package test;
+
+import java.sql.*;
+
+public class TestMySqlJDBC {
+
+    public static void main(String[] args) {
+        var url = "jdbc:mysql://localhost:3306/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conexion = DriverManager.getConnection(url, "root", "3910");
+            Statement instruccion = conexion.createStatement();
+            var sql = "SELECT id_persona, nombre, apellido, email, telefono FROM persona";
+            ResultSet resultado = instruccion.executeQuery(sql);
+            while(resultado.next()){
+                System.out.print("ID persona: " + resultado.getInt("id_persona"));
+                System.out.print(" Nombre: " + resultado.getString("nombre"));
+                System.out.print(" Apellido: " + resultado.getString("apellido"));
+                System.out.print(" Email: " + resultado.getString("email"));
+                System.out.print("  Telefono: " + resultado.getString("telefono"));
+                System.out.println("");
+            }
+            resultado.close();
+            instruccion.close();
+            conexion.close();
+//        } catch (ClassNotFoundException ex) {
+//            ex.printStackTrace(System.out);
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+
+}
