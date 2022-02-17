8ff0fbb3a24629faace64df4a07b80e747885d6d
randymz10 <randymz10@gmail.com>
2022-02-17 11:18:23 -0300

    Adicionado laboratorioo 4 Manejo de transacciones

---
+++ _Laboratorios/04-ManejoTransaccionesJDBC/src/main/java/domain/Usuario.java
@@ -0,0 +1,56 @@
+package domain;
+
+public class Usuario {
+    private int idUsuario;
+    private String username;
+    private String password;
+
+    public Usuario() {
+    }
+
+    public Usuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Usuario(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Usuario(int idUsuario, String username, String password) {
+        this.idUsuario = idUsuario;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "idUsuario=" + idUsuario + ", username=" + username + ", password=" + password + '}';
+    }
+    
+    
+}
---
+++ _Laboratorios/04-ManejoTransaccionesJDBC/src/main/java/test/TestManejoUsuarios.java
@@ -0,0 +1,118 @@
+package test;
+
+import datos.Conexion;
+import datos.UsuarioJDBC;
+import domain.Usuario;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class TestManejoUsuarios {
+
+    public static void main(String[] args) {
+
+        UsuarioJDBC usuarioJDBC = new UsuarioJDBC();
+        Scanner scanner = new Scanner(System.in);
+        boolean salir = false;
+        int opcion;
+
+        while (!salir) {
+            System.out.println(""
+                    + "1. Listar\n"
+                    + "2. Insertar\n"
+                    + "3. Actualizar\n"
+                    + "4. Eliminar\n"
+                    + "5. Salir");
+            System.out.println("Escribe una opcion: ");
+            opcion = scanner.nextInt();
+
+            Connection conexion = null;
+            try {
+                conexion = Conexion.getConnection();
+                if (conexion.getAutoCommit()) {
+                    conexion.setAutoCommit(false);
+                }
+
+                switch (opcion) {
+                    case 1:
+                        List<Usuario> usuarios = usuarioJDBC.seleccionar();
+                        for (Usuario usuario : usuarios) {
+                            System.out.println("usuario = " + usuario);
+                        }
+                        break;
+
+                    case 2:
+                        Usuario insertarUsuario = new Usuario();
+
+                        System.out.println("Inserte el username");
+                        insertarUsuario.setUsername(scanner.next());
+
+                        System.out.println("Inserte el password");
+                        insertarUsuario.setPassword(scanner.next());
+
+                        usuarioJDBC.insertar(insertarUsuario);
+
+                        conexion.commit();
+
+                        System.out.println("Se ha hecho commit de la transaccion");
+                        break;
+
+                    case 3:
+                        Usuario usuarioActualizar = new Usuario();
+
+                        System.out.println("Inserte el id de la persona a modificar: ");
+                        usuarioActualizar.setIdUsuario(scanner.nextInt());
+
+                        System.out.println("Inserte el username de la persona a modificar: ");
+                        usuarioActualizar.setUsername(scanner.next());
+
+                        System.out.println("Inserte el password de la persona a modificar: ");
+                        usuarioActualizar.setPassword(scanner.next());
+
+                        usuarioJDBC.actualizar(usuarioActualizar);
+
+                        conexion.commit();
+
+                        System.out.println("Se ha hecho commit de la transaccion");
+
+                        break;
+
+                    case 4:
+                        Usuario usuarioEliminar = new Usuario();
+
+                        System.out.println("Inserte el ID del usuario a eliminar");
+                        usuarioEliminar.setIdUsuario(scanner.nextInt());
+
+                        usuarioJDBC.eliminar(usuarioEliminar);
+
+                        conexion.commit();
+
+                        System.out.println("Se ha hecho commit de la transaccion");
+                        break;
+
+                    case 5:
+                        salir = true;
+
+                        System.out.println("Adios");
+
+                        break;
+
+                    default:
+                        System.out.println("Opcion incorrecta");
+
+                }
+
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+                System.out.println("Entramos al rollback");
+                try {
+                    conexion.rollback();
+                } catch (SQLException ex1) {
+                    ex.printStackTrace(System.out);
+                }
+            }
+        }
+    }
+}
