86b95503e217263f6ac6151ea84d7dd2af12f11a
randymz10 <randymz10@gmail.com>
2022-06-03 23:38:29 -0300

    Add validation since of the front with js

--- 04-JavaEE/ManejoFormulariosHTML/src/main/webapp/recursos/funciones.js
+++ 04-JavaEE/ManejoFormulariosHTML/src/main/webapp/recursos/funciones.js
@@ -1,2 +1,60 @@
-
-
+function validarForma(forma){
+    var usuario = forma.usuario;
+    if(usuario.value === "" || usuario.value === "Escribir usuario"){
+        alert("Debe proporcionar un nombre de usuario");
+        usuario.focus();
+        usuario.select();
+        
+        return false;
+    }
+    
+    var password = forma.password;
+    if(password.value === "" || password.value.length < 3 ){
+        alert("Debe proporcionar de al menos 3 caracteres");
+        password.focus();
+        password.select();
+        
+        return false;
+    }
+    
+    var tecnologias = forma.tecnologia;
+    var checkSeleccionado = false;
+    
+    for(var i = 0; i < tecnologias.length; i++){
+        if(tecnologias[i].checked){
+            checkSeleccionado = true;
+        }
+        
+        if(!checkSeleccionado){
+            alert("Debe seleccionar una tecnologia");
+            
+            return false;
+        }
+    }
+    
+    var generos = forma.genero;
+    var radioSeleccionado = false;
+    
+    for(var i = 0; generos.length; i++){
+        if(generos[i].checked){
+            radioSeleccionado = true;
+        }
+        
+        if(!radioSeleccionado){
+            alert("Debe Seleccionar un genero");
+            
+            return false;
+        }
+    }
+    
+    var ocupacion = forma.ocupacion;
+    if(ocupacion.value == ""){
+        alert("Debe seleccionar una ocupacion");
+        
+        return false;
+    }
+    
+    //Formulario es valido
+    alert("Formulario valido, enviando datos al servidor");
+    return true;
+}
