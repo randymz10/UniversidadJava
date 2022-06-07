cc244c45210d9ee80aa70640e2c0cafc7df9de41
randymz10 <randymz10@gmail.com>
2022-06-07 11:51:00 -0300

    create Servlets GeneracionExcelServlet and HoraServlet

---
+++ 04-JavaEE/CabecerosRespuesta/src/main/java/web/GeneracionExcelServlet.java
@@ -0,0 +1,13 @@
+package web;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/GeneracionExcelServlet")
+public class GeneracionExcelServlet extends HttpServlet{
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response){
+        
+    }
+}
---
+++ 04-JavaEE/CabecerosRespuesta/src/main/java/web/HoraServlet.java
@@ -0,0 +1,13 @@
+package web;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/HoraServlet")
+public class HoraServlet extends HttpServlet{
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response){
+        
+    }
+}
