ff26db7c210ae20b3a9985402fe09946bd4f8c2c
randymz10 <randymz10@gmail.com>
2022-06-07 21:58:09 -0300

    Add exercise ManejoCookies

---
+++ 04-JavaEE/ManejoCookies/src/main/java/web/CookiesServlet.java
@@ -0,0 +1,46 @@
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("CookiesServlet")
+public class CookiesServlet extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        //supoemos que el usuario visita por primera vez nuestro sitio
+        boolean nuevoUsuario = true;
+        
+        //Obtenemos el arreglo de las cookies
+        Cookie[] cookies = request.getCookies();
+        
+        //Buscamos si ya existe una cookie creada con anterioridad
+        //llamada visitanteRecurrente
+        
+        if(cookies != null){
+            for(Cookie c : cookies){
+                if(c.getName().equals("visitanteRecurrente") && c.getValue().equals("si")){
+                    //si ya existe la cookie es un usuario recurrente
+                    nuevoUsuario = false;
+                    break;
+                }
+            }
+        }
+        
+        String mensaje = null;
+        if(nuevoUsuario){
+            Cookie visitanteCookie = new Cookie("visitanteRecurrente", "si");
+            response.addCookie(visitanteCookie);
+            mensaje = "Gracias por visitar nuestro sitio por primiera vez";
+        }else{
+            mensaje = "Gracias por visitar NUEVAMENTE nuestro sitio";
+        }
+        response.setContentType("text/htmk; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.print("Mensaje: " + mensaje);
+        out.close();
+        
+    }
+}
