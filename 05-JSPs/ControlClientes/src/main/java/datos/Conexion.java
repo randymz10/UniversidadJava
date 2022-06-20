024b97b9b30e92cc7d2320d7321a0f82bb9b4f9d
randymz10 <randymz10@gmail.com>
2022-06-20 20:22:17 -0300

    Project: part 2 ControlClientes( add class Conexion )

---
+++ 05-JSPs/ControlClientes/src/main/java/datos/Conexion.java
@@ -0,0 +1,51 @@
+package datos;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+public class Conexion {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/control_clientes?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASSWORD = "3910";
+    
+    public static DataSource getDataSource(){
+        BasicDataSource ds = new BasicDataSource();
+        ds.setUrl(JDBC_URL);
+        ds.setUsername(JDBC_USER);
+        ds.setPassword(JDBC_PASSWORD);
+        ds.setInitialSize(50);
+        
+        return ds;
+    }
+    
+    public static Connection getConnecion() throws SQLException{
+        return getDataSource().getConnection();
+    }
+    
+    public static void close(ResultSet rs){
+        try {
+            rs.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+    
+    public static void close(PreparedStatement stmt){
+        try {
+            stmt.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+    
+    public static void close(Connection conn){
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+}
