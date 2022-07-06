8626ef46065316928726cdb7dfc8b978db994e4c
randymz10 <randymz10@gmail.com>
2022-07-06 11:49:35 -0300

    Add new exercises

---
+++ 06-JSFs/BolsaTrabajoV6/src/main/java/beans/configuration/ConfigurationJSF.java
@@ -0,0 +1,15 @@
+package beans.configuration;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+import static javax.faces.annotation.FacesConfig.Version.JSF_2_3;
+
+@FacesConfig(
+        //Aciva el CDI build-in beans
+        version = JSF_2_3
+)
+
+@ApplicationScoped
+public class ConfigurationJSF {
+    
+}
