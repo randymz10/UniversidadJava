73964be6f8ee98578fe7cab0143b17dcce5005a4
randymz10 <randymz10@gmail.com>
2022-08-14 00:08:08 -0300

    modify proyect sga-jee-web

--- 07-JavaEE/holamundo-jpa/src/main/java/HolsmundoJpa.java
+++
@@ -1,6 +0,0 @@
-public class HolsmundoJpa {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
--- 07-JavaEE/sga-jee-web/src/main/java/mx/com/gm/sga/domain/Persona.java
+++ 07-JavaEE/sga-jee-web/src/main/java/mx/com/gm/sga/domain/Persona.java
@@ -1,21 +1,33 @@
 package mx.com.gm.sga.domain;
 
 import java.io.Serializable;
+import javax.persistence.*;
 
-
+@Entity
+@NamedQueries({
+    @NamedQuery(name="Persona.findAll", query="SELECT p FROM Persona p ORDER BY p.idPersona")
+})
+@Table(name="persona")
 public class Persona implements Serializable{
     private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id_persona")
     private int idPersona;
+    
     private String nombre;
+    
     private String apellido;
+    
     private String email;
-    private String telefono;
+    
+    private String telefono; 
 
     public Persona() {
     }
 
-    public Persona(int idPersona, String nombre, String apellido, String email, String telefono) {
-        this.idPersona = idPersona;
+    public Persona(String nombre, String apellido, String email, String telefono) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.email = email;
