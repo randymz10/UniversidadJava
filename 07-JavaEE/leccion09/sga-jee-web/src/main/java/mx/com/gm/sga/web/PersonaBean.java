3d5c82801a81084647c62912acadff13386c36c1
randymz10 <randymz10@gmail.com>
2022-10-28 16:04:51 -0300

    Add lesson 09 JAVA EE

---
+++ 07-JavaEE/leccion09/sga-jee-web/src/main/java/mx/com/gm/sga/web/PersonaBean.java
@@ -0,0 +1,60 @@
+package mx.com.gm.sga.web;
+
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.*;
+import mx.com.gm.sga.domain.Persona;
+import mx.com.gm.sga.servicio.PersonaService;
+import org.apache.logging.log4j.*;
+import org.primefaces.event.RowEditEvent;
+
+@Named("personaBean")
+@RequestScoped
+public class PersonaBean {
+
+    Logger log = LogManager.getRootLogger();
+
+    @Inject
+    private PersonaService personaService;
+
+    private Persona personaSeleccionada;
+
+    List<Persona> personas;
+
+    public PersonaBean() {
+        log.debug("Iniciando e objeto PersonaBean");
+    }
+
+    ;
+    
+    @PostConstruct
+    public void inicializar() {
+        //Iniciamos las variables
+        this.personas = personaService.listarPersonas();
+        log.debug("personas recuperadasen ManagedBean: " + this.personas);
+        personaSeleccionada = new Persona();
+    }
+
+    public void editListener(RowEditEvent event) {
+        Persona persona = (Persona) event.getObject();
+        personaService.modificarPersona(persona);
+    }
+
+    public Persona getPersonaSeleccionada() {
+        return personaSeleccionada;
+    }
+
+    public void setPersonaSeleccionada(Persona personaSeleccionada) {
+        this.personaSeleccionada = personaSeleccionada;
+    }
+
+    public List<Persona> getPersonas() {
+        return personas;
+    }
+
+    public void setPersonas(List<Persona> personas) {
+        this.personas = personas;
+    }
+
+}
