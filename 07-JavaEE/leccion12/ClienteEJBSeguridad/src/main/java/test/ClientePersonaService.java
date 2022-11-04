6e1ff770a26167f6b1ff38c45a66add0396ccfa1
randymz10 <randymz10@gmail.com>
2022-11-04 14:45:40 -0300

    Finished JavaEE

---
+++ 07-JavaEE/leccion12/ClienteEJBSeguridad/src/main/java/test/ClientePersonaService.java
@@ -0,0 +1,37 @@
+package test;
+
+import com.sun.enterprise.security.ee.auth.login.ProgrammaticLogin;
+import java.util.List;
+import javax.naming.*;
+import mx.com.gm.sga.domain.Persona;
+import mx.com.gm.sga.servicio.PersonaServiceRemote;
+
+public class ClientePersonaService {
+    public static void main(String[] args) {
+        System.out.println("Iniciando llamada al EJB desde el cliente");
+        
+        String authFile = "login.conf";
+        
+        System.setProperty("java.scurity.auth.login.config", authFile);
+        ProgrammaticLogin programmaticLogin = new ProgrammaticLogin();
+        
+        programmaticLogin.login("admin", "admin".toCharArray());
+        
+        try {
+            Context jndi = new InitialContext();
+            PersonaServiceRemote personaService = (PersonaServiceRemote) jndi.lookup("java:global/sga-jee-web/PersonaServiceImpl!mx.com.gm.sga.servicio.PersonaServiceRemote");
+            
+            List<Persona> personas = personaService.listarPersonas();
+            
+            for(Persona persona: personas){
+                System.out.println("Persona: " + persona);
+            }
+            
+            System.out.println("\nFin llamada al EJB desde el cliente");
+        } catch (NamingException ex) {
+            ex.printStackTrace(System.out);
+        }
+        
+    }
+   
+}
