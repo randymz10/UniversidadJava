e363d4013648d60d4143f522d5e562ed149612c1
randymz10 <randymz10@gmail.com>
2022-11-09 16:22:50 -0300

    Add new exercise of Struts2

---
+++ 08-Struts/leccion04/PersonasStruts2/src/main/java/beans/actions/MostrarPersonaAction.java
@@ -0,0 +1,45 @@
+package beans.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.logging.log4j.*;
+
+public class MostrarPersonaAction extends ActionSupport {
+    
+    Logger log = LogManager.getLogger(MostrarPersonaAction.class);
+    
+    private String nombre;
+    
+    public String execute(){
+        log.info("El nombre es: " + this.nombre);
+        return SUCCESS;
+    }
+
+    public Logger getLog() {
+        return log;
+    }
+
+    public void setLog(Logger log) {
+        this.log = log;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String getTitulo(){
+        return getText("persona.titulo");
+    }
+    
+    public String getValor(){
+        return getText("persona.valor");
+    }
+    
+    public String getBoton() {
+        return getText("persona.boton");
+    }
+    
+}
