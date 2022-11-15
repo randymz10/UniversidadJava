439d23a3c8f3c7a89aa7e0ee28a1909dd3eb8cbe
randymz10 <randymz10@gmail.com>
2022-11-15 20:16:10 -0300

    Add lesson 07 struts2

--- 08-Struts/leccion07/LoginStruts2/src/main/java/beans/actions/ValidarUsuarioAction.java
+++ 08-Struts/leccion07/LoginStruts2/src/main/java/beans/actions/ValidarUsuarioAction.java
@@ -6,8 +6,8 @@ import org.apache.logging.log4j.*;
 import org.apache.struts2.convention.annotation.*;
 
 @Results({
-    @Result(name = "success", location = "/WEB-INF/content/bienvenido.jsp"),
-    @Result(name = "input", location = "/WEB-INF/content/login.jsp")
+    @Result(name="success", location="/WEB-INF/content/bienvenido.jsp"),
+    @Result(name="input", location="/WEB-INF/content/login.jsp")
 })
 public class ValidarUsuarioAction extends ActionSupport {
 
@@ -25,24 +25,25 @@ public class ValidarUsuarioAction extends ActionSupport {
             return INPUT;
         }
     }
-
+    
     @Override
-    public void validate() {
-        if (this.usuario == null || "".equals(this.usuario.trim())) {
-            addFieldError("usuario", getText("val.usuario"));
-        } else if (!usuarioValido()) {
+    public void validate(){
+        if(this.usuario == null || "".equals(this.usuario.trim())){
+            addFieldError("usuario",getText("val.usuario"));
+        }
+        else if(!usuarioValido()){
             addActionError(getText("usuario.invalido"));
         }
-
-        if (this.password == null || "".equals(this.password.trim())) {
+        
+        if(this.password == null || "".equals(this.password.trim())){
             addFieldError("password", getText("val.password"));
         }
-        else if(this.password.length() < 3) {
-            addFieldError("password", getText("val.pass.min.length"));
+        else if(this.password.length() < 3){
+            addFieldError("password", getText("val.pass.min.lenght"));
         }
     }
-
-    private boolean usuarioValido() {
+    
+    private boolean usuarioValido(){
         return "admin".equals(this.usuario);
     }
 
