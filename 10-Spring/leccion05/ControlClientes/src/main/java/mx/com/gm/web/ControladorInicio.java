dd3e5e9e4f308be53ccaae45905524d66dd95769
randymz10 <randymz10@gmail.com>
2022-12-05 09:29:24 -0300

    add lesso04 spring

---
+++ 10-Spring/leccion05/ControlClientes/src/main/java/mx/com/gm/web/ControladorInicio.java
@@ -0,0 +1,25 @@
+package mx.com.gm.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import mx.com.gm.servicio.PersonaService;
+
+@Controller
+@Slf4j
+public class ControladorInicio {
+    
+    @Autowired
+    private PersonaService personaService;
+
+    @GetMapping("/")
+    public String inicio(Model model) {
+        var personas = personaService.listarPerssonas();
+       
+        log.info("ejecutando el controlador Spring MVC");
+        model.addAttribute("personas", personas);
+        return "index";
+    }
+}
