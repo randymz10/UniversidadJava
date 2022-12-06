4f183bc4c4c8ba02a992b5c2cdc8c32c7e0892de
randymz10 <randymz10@gmail.com>
2022-12-06 13:26:56 -0300

    Add lesso06 spring

---
+++ 10-Spring/leccion06/ControlClientes/src/main/java/mx/com/gm/web/ControladorInicio.java
@@ -0,0 +1,56 @@
+package mx.com.gm.web;
+
+import javax.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import mx.com.gm.domain.Persona;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import mx.com.gm.servicio.PersonaService;
+import org.springframework.validation.Errors;
+
+@Controller
+@Slf4j
+public class ControladorInicio {
+    
+    @Autowired
+    private PersonaService personaService;
+
+    @GetMapping("/")
+    public String inicio(Model model) {
+        var personas = personaService.listarPerssonas();
+       
+        log.info("ejecutando el controlador Spring MVC");
+        model.addAttribute("personas", personas);
+        return "index";
+    }
+    
+    @GetMapping("/agregar")
+    public String agregar(Persona persona){
+        return "modificar";
+    }
+    
+    @PostMapping("/guardar")
+    public String guardar(@Valid Persona persona, Errors errores) {
+        if(errores.hasErrors()){
+            return "modificar";
+        }
+        personaService.guardar(persona);
+        return "redirect:/";
+    }
+    
+    @GetMapping("/editar/{idPersona}")
+    public String editar(Persona persona, Model model) {
+        persona = personaService.encontrarPersona(persona);
+        model.addAttribute("persona", persona);
+        return "modificar";
+    }
+    
+    //@GetMapping("/eliminar/{idPersona}")
+    @GetMapping("/eliminar")
+    public String eliminar(Persona persona) {
+        personaService.eliminar(persona);
+        return "redirect:/";
+    }
+}
