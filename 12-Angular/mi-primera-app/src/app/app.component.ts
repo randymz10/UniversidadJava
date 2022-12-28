66c01751fdb71c704648112b0258ba7c16e3a282
randymz10 <randymz10@gmail.com>
2022-12-28 15:16:19 -0300

    finished section fundamentos de Angular

--- 12-Angular/mi-primera-app/src/app/app.component.ts
+++ 12-Angular/mi-primera-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Mi primera App en Angular';
+  titulo = 'Hola Mundo desde Angular';
 }
